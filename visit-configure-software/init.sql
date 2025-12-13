d1190c5d6e0a8cf06b27d7f10346fdf70831eb9a
Ganesh S Acharya <33441668+khazaddoom@users.noreply.github.com>
2025-12-13 11:41:52 +0000

    Added more Tables

--- visit-configure-software/init.sql
+++ visit-configure-software/init.sql
@@ -1,57 +1,65 @@
 // Patients table
 
 CREATE TABLE IF NOT EXISTS PATIENT (
-    patient_id INTEGER NOT NULL PRIMARY KEY
-    first_name TEXT NOT NULL
-    last_name TEXT NOT NULL
-    bed_code TEXT NOT NULL
-    createAt DATETIME
+    patient_id INTEGER NOT NULL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    bed_code TEXT NOT NULL,
+    createAt DATETIME,
     updatedAy DATETIME
 )
 
 CREATE TABLE IF NOT EXISTS ADMISSION (
-    admission_id INTEGER NOT NULL PRIMARY KEY
-    patient_id INTEGER NOT NULL
-    admittedOn DATETIME NOT NULL
-    dignosis TEXT 
-    attending_doctor_id INTEGER NOT NULL
-    createAt DATETIME
+    admission_id INTEGER NOT NULL PRIMARY KEY,
+    patient_id INTEGER NOT NULL,
+    admittedOn DATETIME NOT NULL,
+    dignosis TEXT,
+    attending_doctor_id INTEGER NOT NULL,
+    createAt DATETIME,
     updatedAy DATETIME
 )
 
 
 CREATE TABLE IF NOT EXISTS BED (
     bed_id TEXT NOT NULL PRIMARY KEY,
-    room_id TEXT NOT NULL
-    createAt DATETIME
+    room_id TEXT NOT NULL,
+    createAt DATETIME,
     updatedAy DATETIME
 )
 
 CREATE TABLE IF NOT EXISTS ROOM (
     room_id TEXT NOT NULL PRIMARY KEY,
-    floor_id TEXT NOT NULL
-    createAt DATETIME
+    floor_id TEXT NOT NULL,
+    createAt DATETIME,
     updatedAy DATETIME
 )
 
 CREATE TABLE IF NOT EXISTS FLOOR (
     floor_id TEXT NOT NULL PRIMARY KEY,
-    building_id TEXT NOT NULL
-    createAt DATETIME
+    building_id TEXT NOT NULL,
+    createAt DATETIME,
     updatedAy DATETIME
 )
 
 CREATE TABLE IF NOT EXISTS BUILDING (
     building_id TEXT NOT NULL PRIMARY KEY,
-    facility_id TEXT NOT NULL
-    createAt DATETIME
+    facility_id TEXT NOT NULL,
+    createAt DATETIME,
     updatedAy DATETIME
 )
 
 CREATE TABLE IF NOT EXISTS FACILITY (
     facility_id TEXT NOT NULL PRIMARY KEY,
-    facility_location TEXT NOT NULL
-    createAt DATETIME
+    facility_location TEXT NOT NULL,
+    createAt DATETIME,
     updatedAy DATETIME
 )
+
+CREATE TABLE IF NOT EXISTS CONFIGURATION (
+    configuration_id TEXT NOT NULL PRIMARY KEY,
+    description TEXT,
+    createAt DATETIME,
+    updatedAy DATETIME
+)
+
 
